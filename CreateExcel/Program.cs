70d1716c77e3b96191ec44fcf30b2a1da16a6d8d
bosheng chen <bosheng.chen@infosys.com>
2019-09-18 16:47:13 +0800

    Add project files.

---
+++ CreateExcel/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using NPOI.HSSF.UserModel;
+using NPOI.HPSF;
+using NPOI.POIFS.FileSystem;
+using NPOI.SS.UserModel;
+using Bogus;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace CreateExcel
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset Now { get; set; }
+        public bool New { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Randomizer.Seed = new Random(8675309);
+
+            var faker = new Faker<Model>()
+                .RuleFor(x => x.Id, f => f.Random.Int(0, 100))
+                .RuleFor(x => x.Name, f => f.Name.FullName())
+                .RuleFor(x => x.Now, f =>
+                {
+                    var date = f.Date.Past();
+                    return new DateTimeOffset(date);
+                })
+                .RuleFor(x => x.New, f => f.Random.Bool())
+            ;
+
+
+            var lists = faker.Generate(10);
+
+            HSSFWorkbook hssfworkbook;
+
+            // InitializeWorkbook
+            hssfworkbook = new HSSFWorkbook();
+
+            //Create a entry of DocumentSummaryInformation
+            DocumentSummaryInformation dsi = PropertySetFactory.CreateDocumentSummaryInformation();
+            dsi.Company = "NPOI Team";
+            hssfworkbook.DocumentSummaryInformation = dsi;
+
+            //Create a entry of SummaryInformation
+            SummaryInformation si = PropertySetFactory.CreateSummaryInformation();
+            si.Subject = "NPOI SDK Example";
+            hssfworkbook.SummaryInformation = si;
+
+            ISheet sheet = hssfworkbook.CreateSheet(typeof(Model).Name);
+
+            var properties = typeof(Model).GetProperties();
+
+            //Header
+            var header = sheet.CreateRow(0);
+            for (var i = 0; i < properties.Length; i++)
+            {
+                var cell = header.CreateCell(i);
+                cell.SetCellValue(properties[i].Name);
+            }
+
+            //body
+
+            IRow sheetRow = null;
+
+            for (int i = 0; i < lists.Count; i++)
+            {
+                sheetRow = sheet.CreateRow(i + 1);
+
+                for (var j = 0; j < properties.Length; j++)
+                {
+                    ICell Row1 = sheetRow.CreateCell(j);
+                    //string cellvalue = FormatRow(properties[j], lists[i]);
+                    //Row1.SetCellValue(cellvalue);
+                    SetCell(hssfworkbook, Row1, properties[j], lists[i]);
+                }
+            }
+
+            FileStream file = new FileStream(@"test.xls", FileMode.Create);
+            hssfworkbook.Write(file);
+            file.Close();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void SetCell(HSSFWorkbook hssfworkbook, ICell cell, PropertyInfo propertyInfo, Model model)
+        {
+            ICellStyle cellStyle = hssfworkbook.CreateCellStyle();
+            IDataFormat format = hssfworkbook.CreateDataFormat();
+            cellStyle.DataFormat = format.GetFormat("yyyy-MM-dd HH:mm:ss");
+
+            var value = propertyInfo.GetValue(model);
+            switch (value)
+            {
+                case string s:
+                    cell.SetCellValue($"{s.Replace("\"", "\"\"")}");
+                    break;
+                case DateTimeOffset dto:
+                    cell.SetCellValue(dto.UtcDateTime);
+                    cell.CellStyle = cellStyle;
+                    break;
+                case DateTime dto:
+                    cell.SetCellValue(dto);
+                    break;
+                case bool b:
+                    cell.SetCellValue(b);
+                    break;
+                default:
+                    cell.SetCellValue(Convert.ToString(value));
+                    break;
+            }
+        }
+
+        private static string FormatRow(PropertyInfo propertyInfo, Model model)
+        {
+            var value = propertyInfo.GetValue(model);
+            switch (value)
+            {
+                case string s: return $"\"{s.Replace("\"", "\"\"")}\"";
+                case DateTimeOffset dto: return dto.ToString("yyyy-MM-dd HH:mm:ss");
+                case bool b: return b ? "True" : "False";
+                default: return Convert.ToString(value);
+            }
+        }
+    }
+}
